5edc10e1b34fab4165f3c48bf0351bd4f64b46a2
Marvin Schopf <marvin@schopf.biz>
2021-02-07 16:24:40 +0100

    Fix & add build

--- src/index.ts
+++ src/index.ts
@@ -21,11 +21,11 @@
 
 import { promises as fs } from "fs";
 
-export default async function exists(path: string): Promise<boolean> {
+export = async (path: string): Promise<boolean> => {
 	try {
 		await fs.stat(path);
 	} catch (e: unknown) {
 		return false;
 	}
 	return true;
-}
+};
